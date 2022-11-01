fdcb5be761afcfa7cfdfc116e56173d0f20b9f0a
Hypexel <114844376+Hypexel@users.noreply.github.com>
2022-11-01 17:14:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    role_name = ""
+    role_name = "Member"
     message_id = ""
-    prefix = "."
-    bot_token = ""
+    prefix = "!"
+    bot_token = "OTcxMDQ4OTU1ODgwMjgwMTE1.GyHVY9.I_vUmskN7Ht2JfK0SipSyomApDTTIBf_xnDPQw"
     channel_id = ""
